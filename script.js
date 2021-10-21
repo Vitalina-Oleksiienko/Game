e411f7b6d7c3514dca4809f6a6ea0eece929fd74
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2021-10-21 16:28:49 +0300

    starrt

---
+++ script.js
@@ -0,0 +1,13 @@
+let start  = document.querySelector("#start button");
+ start.style.background = "green";
+ //start.style.padding = "120px 160px";
+
+console.dir(start);
+
+let lifes = document.querySelector("#start lifes");
+
+console.dir(lifes);
+
+let score = document.querySelector("#start score");
+
+console.dir(score);
